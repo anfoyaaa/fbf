91633aee0cea0eb34d8028261ecd6c339bb45a93
anfoyaaa <124536077+anfoyaaa@users.noreply.github.com>
2023-02-06 23:23:02 +0300

    Mdk 04

---
+++ Program interface Printable.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+interface  Printable  {
+
+     void Print();
+}
+
+class Book: Printable{
+
+    public void Print()
+    {
+        Console.WriteLine("books");
+    }
+    public static void printMagazines(Printable[] printables)
+    {
+
+        foreach (Printable printable in printables)
+        {
+            if (printable is Book)
+            {
+                Console.WriteLine("print only boobs");
+            }
+        }
+
+    }
+}
+
+
+class Magazine: Printable
+{
+
+    public void Print()
+
+    {
+        Console.WriteLine(" Magazine");
+    }
+    public static void printMagazines(Printable[] printables)
+    {
+
+        foreach (Printable printable in printables)
+        {
+            if (printable is Book)
+            {
+                Console.WriteLine("print only Magazines");
+            }
+        }
+
+    }
+}
+
+class clas
+{
+    static void Main(string[] args) {
+        Printable[] printables = new Printable[] { new Book(), new Magazine() };
+        foreach (Printable printable in printables) {
+            printable.Print();
+        }
+        Book.printBooks(printables);
+        Magazine.printMagazines(printables);
+    }
+}
---
+++ interface Instrument.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+interface Instrument
+{
+    void play();
+    string KEY = "До мажор";
+
+ 
+}
+
+class Guitar: Instrument
+{
+    int stringCount;
+    public void play()
+    {
+        Console.WriteLine("Играет гитара, количество струн: 5");
+    }
+
+}
+
+class Drums: Instrument
+{
+    int size ;
+    public void play()
+    {
+        Console.WriteLine("Играет барабан, размером: 54");
+    }
+}
+
+class Wind: Instrument
+{
+    int diametr;
+    public void play()
+    {
+        Console.WriteLine("Играет труба, диаметрос: 5");
+    }
+
+    class clas
+    {
+        static void Main(string[] args)
+        {
+            Instrument[] instruments = { new Guitar { stringCount = 54 }, new Drums { size = 54 }, new Wind { diametr = 5 } };
+            foreach (var instrument in instruments)
+            {
+                instrument.play();
+            }
+        }
+        
+    }
